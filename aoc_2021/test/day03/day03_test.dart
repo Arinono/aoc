4709e64bf61c42d7974bd14cddd23df16e02993d
Aurelien Arino <aurelien.arino@gmail.com>
2021-12-03 18:15:58 +0100

    feat(2021_03): part 2

--- aoc_2021/test/day03/day03_test.dart
+++ aoc_2021/test/day03/day03_test.dart
@@ -2,6 +2,20 @@ import 'package:aoc_2021/day03/day03.dart';
 import 'package:test/test.dart';
 
 void main() {
+  final List<String> testInput = List.from([
+    '00100',
+    '11110',
+    '10110',
+    '10111',
+    '10101',
+    '01111',
+    '00111',
+    '11100',
+    '10000',
+    '11001',
+    '00010',
+    '01010',
+  ]);
   group('\nday03\n', () {
     group('given I have a diagnostic report\n', () {
       group('if the report is empty\n', () {
@@ -10,26 +24,15 @@ void main() {
           final List<String> report = List.empty();
 
           // act/assert
-          expect(() => SubmarineDiagnostic.from(report),
-              throwsA(isA<SubmarineDianosticEmptyException>()));
+          expect(
+            () => SubmarineDiagnostic.from(report),
+            throwsA(isA<SubmarineDianosticEmptyException>()),
+          );
         });
       });
       test('I determine the gamma rate\n', () {
         // arrange
-        final List<String> report = List.from([
-          '00100',
-          '11110',
-          '10110',
-          '10111',
-          '10101',
-          '01111',
-          '00111',
-          '11100',
-          '10000',
-          '11001',
-          '00010',
-          '01010',
-        ]);
+        final List<String> report = testInput;
 
         // act
         final SubmarineDiagnostic diag = SubmarineDiagnostic.from(report);
@@ -41,20 +44,7 @@ void main() {
 
       test('I determine the epsilon rate\n', () {
         // arrange
-        final List<String> report = List.from([
-          '00100',
-          '11110',
-          '10110',
-          '10111',
-          '10101',
-          '01111',
-          '00111',
-          '11100',
-          '10000',
-          '11001',
-          '00010',
-          '01010',
-        ]);
+        final List<String> report = testInput;
 
         // act
         final SubmarineDiagnostic diag = SubmarineDiagnostic.from(report);
@@ -62,6 +52,30 @@ void main() {
         // assert
         expect(diag.rates.epsilon.toString(), equals('01001'));
         expect(diag.rates.epsilon.toDec(), equals(9));
+      });
+
+      test('I determine the oxygen generator rate\n', () {
+        // arrange
+        final List<String> report = testInput;
+
+        // act
+        final SubmarineDiagnostic diag = SubmarineDiagnostic.from(report);
+
+        // assert
+        expect(diag.rates.oxyGenerator.toString(), equals('10111'));
+        expect(diag.rates.oxyGenerator.toDec(), equals(23));
+      });
+
+      test('I determine the CO2 scrubber rate\n', () {
+        // arrange
+        final List<String> report = testInput;
+
+        // act
+        final SubmarineDiagnostic diag = SubmarineDiagnostic.from(report);
+
+        // assert
+        expect(diag.rates.diOxyScrubber.toString(), equals('01010'));
+        expect(diag.rates.diOxyScrubber.toDec(), equals(10));
       });
     });
   });
