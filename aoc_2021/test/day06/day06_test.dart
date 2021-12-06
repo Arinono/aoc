ff014b29d9caa4eaf455555d9ed3104fc438a8f8
Aurelien Arino <aurelien.arino@gmail.com>
2021-12-06 12:59:48 +0100

    feat(2021_06): part 2

--- aoc_2021/test/day06/day06_test.dart
+++ aoc_2021/test/day06/day06_test.dart
@@ -3,71 +3,41 @@ import 'package:test/test.dart';
 
 void main() {
   group('\nday06\n', () {
-    group('given I have a Fish\n', () {
-      group('when I wait for a day\n', () {
-        test('its internal timer decrements by 1\n', () {
-          // arrange
-          final Fish fish = Fish.from(3);
-
-          // act
-          fish.wait();
-
-          // assert
-          expect(fish.timer, equals(2));
-        });
-
-        group('and its timer is at 0\n', () {
-          test('it spawns a new fish\n', () {
-            // arrange
-            final Fish fish = Fish.from(0);
-
-            // act
-            final Fish? newFish = fish.wait();
-
-            // assert
-            expect(fish.timer, equals(6));
-            expect(newFish, isA<Fish>());
-            expect(newFish!.timer, equals(8));
-          });
-        });
-      });
-    });
-
     group('given I have multiple fishes\n', () {
       group('when I wait 18 days\n', () {
         test('I end up with 26 fishes\n', () {
           // arrange
           final List<Fish> fishes = List.from([
-            Fish.from(3),
-            Fish.from(4),
-            Fish.from(3),
-            Fish.from(1),
-            Fish.from(2),
+            3,
+            4,
+            3,
+            1,
+            2,
           ]);
 
           // act
-          final List<Fish> finalFishes = fishes.waitFor(18);
+          final int fishesNumber = fishes.waitFor(18);
 
           // assert
-          expect(finalFishes.length, equals(26));
+          expect(fishesNumber, equals(26));
         });
       });
       group('when I wait 80 days\n', () {
         test('I end up with 5934 fishes\n', () {
           // arrange
           final List<Fish> fishes = List.from([
-            Fish.from(3),
-            Fish.from(4),
-            Fish.from(3),
-            Fish.from(1),
-            Fish.from(2),
+            3,
+            4,
+            3,
+            1,
+            2,
           ]);
 
           // act
-          final List<Fish> finalFishes = fishes.waitFor(80);
+          final int fishesNumber = fishes.waitFor(80);
 
           // assert
-          expect(finalFishes.length, equals(5934));
+          expect(fishesNumber, equals(5934));
         });
       });
     });
