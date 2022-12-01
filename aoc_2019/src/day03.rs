49dd96d61a576e08f9941a1cc68e7f9c23ce630b
Aurelien Arino <aurelien.arino@gmail.com>
2022-12-01 23:00:38 +0100

    wip(2019.3)

--- aoc_2019/src/day03.rs
+++ aoc_2019/src/day03.rs
@@ -9,7 +9,7 @@ enum Direction {
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, PartialOrd, Ord, Hash)]
-struct Point(i32, i32);
+struct Point(i32, i32, i32);
 
 fn parse_wire(input: String) -> Vec<Direction> {
     input
@@ -34,7 +34,7 @@ fn spread_line(from: &Point, dir: &Direction) -> (HashSet<Point>, Point) {
         Direction::Up(by) => {
             let iby: i32 = by.into();
             let pts = (from.1 + 1..=from.1 + iby).fold(HashSet::new(), |mut acc, y| {
-                let pt = Point(from.0, y);
+                let pt = Point(from.0, y, last.2 + 1);
                 last = pt.clone();
                 acc.insert(pt);
                 acc
@@ -46,7 +46,7 @@ fn spread_line(from: &Point, dir: &Direction) -> (HashSet<Point>, Point) {
             let pts = (from.1 - iby..from.1)
                 .rev()
                 .fold(HashSet::new(), |mut acc, y| {
-                    let pt = Point(from.0, y);
+                    let pt = Point(from.0, y, last.2 + 1);
                     last = pt.clone();
                     acc.insert(pt);
                     acc
@@ -56,7 +56,7 @@ fn spread_line(from: &Point, dir: &Direction) -> (HashSet<Point>, Point) {
         Direction::Right(by) => {
             let iby: i32 = by.into();
             let pts = (from.0 + 1..=from.0 + iby).fold(HashSet::new(), |mut acc, x| {
-                let pt = Point(x, from.1);
+                let pt = Point(x, from.1, last.2 + 1);
                 last = pt.clone();
                 acc.insert(pt);
                 acc
@@ -68,7 +68,7 @@ fn spread_line(from: &Point, dir: &Direction) -> (HashSet<Point>, Point) {
             let pts = (from.0 - iby..from.0)
                 .rev()
                 .fold(HashSet::new(), |mut acc, x| {
-                    let pt = Point(x, from.1);
+                    let pt = Point(x, from.1, last.2 + 1);
                     last = pt.clone();
                     acc.insert(pt);
                     acc
@@ -79,7 +79,7 @@ fn spread_line(from: &Point, dir: &Direction) -> (HashSet<Point>, Point) {
 }
 
 fn get_wire_points(line: &Vec<Direction>) -> HashSet<Point> {
-    let mut prev = Point(0, 0);
+    let mut prev = Point(0, 0, 0);
     line.iter().fold(HashSet::from([]), |mut acc, dir| {
         let (pts, new_prev) = spread_line(&prev, dir);
         prev = new_prev;
@@ -100,6 +100,14 @@ fn find_intersections<'a>(wires: &'a Vec<HashSet<Point>>) -> HashSet<&'a Point> 
         .first()
         .expect("to have only one")
         .intersection(wire2.first().expect("to have only one"))
+        .map(|i| {
+            println!("{:?}", wire1.first().unwrap().clone().retain(|j| {
+                println!("{:?}", j);
+                j.0 == i.0 && j.1 == i.1
+            }));
+            return i;
+        })
+        // do smth with the instersection
         .collect::<HashSet<_>>()
 }
 
@@ -152,30 +160,30 @@ mod tests {
             points,
             HashSet::from([
                 // right by 8
-                Point(1, 0),
-                Point(2, 0),
-                Point(3, 0),
-                Point(4, 0),
-                Point(5, 0),
-                Point(6, 0),
-                Point(7, 0),
-                Point(8, 0),
+                Point(1, 0, 1),
+                Point(2, 0, 2),
+                Point(3, 0, 3),
+                Point(4, 0, 4),
+                Point(5, 0, 5),
+                Point(6, 0, 6),
+                Point(7, 0, 7),
+                Point(8, 0, 8),
                 // up by 5
-                Point(8, 1),
-                Point(8, 2),
-                Point(8, 3),
-                Point(8, 4),
-                Point(8, 5),
+                Point(8, 1, 9),
+                Point(8, 2, 10),
+                Point(8, 3, 11),
+                Point(8, 4, 12),
+                Point(8, 5, 13),
                 // left by 5
-                Point(7, 5),
-                Point(6, 5),
-                Point(5, 5),
-                Point(4, 5),
-                Point(3, 5),
+                Point(7, 5, 14),
+                Point(6, 5, 15),
+                Point(5, 5, 16),
+                Point(4, 5, 17),
+                Point(3, 5, 18),
                 // down by 3
-                Point(3, 4),
-                Point(3, 3),
-                Point(3, 2),
+                Point(3, 4, 19),
+                Point(3, 3, 20),
+                Point(3, 2, 21),
             ])
         );
     }
@@ -188,7 +196,7 @@ mod tests {
 
         let intersections = find_intersections(&wires);
 
-        assert_eq!(intersections, HashSet::from([&Point(3, 3), &Point(6, 5)]));
+        assert_eq!(intersections, HashSet::from([&Point(3, 3, 40), &Point(6, 5, 30)]));
     }
 
     #[test]
@@ -219,6 +227,36 @@ mod tests {
         assert_eq!(distance1, 6);
         assert_eq!(distance2, 159);
         assert_eq!(distance3, 135);
+    }
+
+    #[test]
+    fn test_calc_most_efficient() {
+        let wires1 = vec![
+            get_wire_points(&parse_wire(String::from("R8,U5,L5,D3"))),
+            get_wire_points(&parse_wire(String::from("U7,R6,D4,L4"))),
+        ];
+        let wires2 = vec![
+            get_wire_points(&parse_wire(String::from(
+                "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+            ))),
+            get_wire_points(&parse_wire(String::from("U62,R66,U55,R34,D71,R55,D58,R83"))),
+        ];
+        let wires3 = vec![
+            get_wire_points(&parse_wire(String::from(
+                "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+            ))),
+            get_wire_points(&parse_wire(String::from(
+                "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+            ))),
+        ];
+
+        let distance1 = calc_distance(find_intersections(&wires1));
+        let distance2 = calc_distance(find_intersections(&wires2));
+        let distance3 = calc_distance(find_intersections(&wires3));
+
+        assert_eq!(distance1, 30);
+        assert_eq!(distance2, 610);
+        assert_eq!(distance3, 410);
     }
 }
 
