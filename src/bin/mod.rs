9ddf6b7a558e1ac58268b682990d387fe1811b60
Aurelien Arino <aurelien@arino.io>
2023-12-03 11:55:59 +0100

    rearrange bin

--- src/bin/mod.rs
+++ src/bin/mod.rs
@@ -1,51 +1,111 @@
 extern crate aoc;
 
 use anyhow::Result;
-use aoc::*;
 use clap::Parser;
-use std::fs;
+use std::{fmt::Display, fs, str::FromStr};
 
 #[derive(Parser)]
 #[command(author, version)]
 struct Cli {
     #[arg(short, long)]
-    day: u32,
+    day: Option<Day>,
 
     #[arg(short, long)]
-    part: Option<u32>,
+    part: Option<Part>,
+}
+
+#[derive(Debug, Clone)]
+struct Day(u32);
+
+impl FromStr for Day {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let day = s.parse::<u32>()?;
+        if day < 1 || day > 25 {
+            return Err(anyhow::anyhow!("Invalid day"));
+        }
+        Ok(Day(day))
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Part(u32);
+
+impl FromStr for Part {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let part = s.parse::<u32>()?;
+        if part != 1 && part != 2 {
+            return Err(anyhow::anyhow!("Invalid part"));
+        }
+        Ok(Part(part))
+    }
+}
+
+impl Day {
+    fn as_padded(&self) -> String {
+        format!("{:0>2}", self.0)
+    }
+}
+
+impl Display for Day {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.as_padded())
+    }
+}
+
+trait Run {
+    fn run(&self, part: Part) -> Result<String>;
+}
+
+impl Run for Day {
+    fn run(&self, part: Part) -> Result<String> {
+        let input = fs::read_to_string(format!("./inputs/day{}.txt", self.as_padded()))?;
+
+        Ok(match self.0 {
+            1 => match part.0 {
+                1 => aoc::day01::part1::solve(&input),
+                2 => aoc::day01::part2::solve(&input),
+                _ => format!(
+                    "{}\n\t{}",
+                    aoc::day01::part1::solve(&input),
+                    aoc::day01::part2::solve(&input),
+                ),
+            },
+            2 => match part.0 {
+                1 => aoc::day02::part1::solve(&input),
+                2 => aoc::day02::part2::solve(&input),
+                _ => format!(
+                    "{}\n\t{}",
+                    aoc::day02::part1::solve(&input),
+                    aoc::day02::part2::solve(&input),
+                ),
+            },
+            _ => todo!(),
+        })
+    }
 }
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
-    let day = cli.day;
-    let padded_day = format!("{:0>2}", day);
 
-    let input = fs::read_to_string(format!("./inputs/day{}.txt", &padded_day))?;
+    if let Some(day) = cli.day {
+        if let Some(part) = cli.part {
+            println!("Day {}:", day);
+            println!("\t{}", day.run(part)?);
+            return Ok(());
+        }
 
-    match day {
-        1 => match cli.part {
-            Some(1) => println!("{}", day01::part1::solve(&input)),
-            Some(2) => println!("{}", day01::part2::solve(&input)),
-            None => {
-                println!("{}", day01::part1::solve(&input));
-                println!("{}", day01::part2::solve(&input));
-            }
-            Some(_) => {
-                return Err(anyhow::anyhow!("Invalid part number"));
-            }
-        },
-        2 => match cli.part {
-            Some(1) => println!("{}", day02::part1::solve(&input)),
-            Some(2) => println!("{}", day02::part2::solve(&input)),
-            None => {
-                println!("{}", day02::part1::solve(&input));
-                println!("{}", day02::part2::solve(&input));
-            }
-            Some(_) => {
-                return Err(anyhow::anyhow!("Invalid part number"));
-            }
-        },
-        _ => todo!(),
+        println!("Day {}:", day);
+        println!("\t{}", day.run(Part(0))?);
+        return Ok(());
+    }
+
+    for day in 1..=25 {
+        println!("Day {}:", Day(day));
+        println!("\t{}", Day(day).run(Part(0))?);
     }
 
     Ok(())
